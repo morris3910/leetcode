d7f6d4aeef97ff668cc6d6c19efe92d7b194ecf8
Morris1216 <kjhsieh.ii14@nycu.edu.tw>
2026-05-17 22:25:54 +0800

    update

---
+++ Week10/330_Patching-Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        int patch = 0, i = 0;
+        long long miss = 1;
+        
+        while (miss <= n) {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                patch++;
+            }
+        }
+        return patch;
+    }
+};
